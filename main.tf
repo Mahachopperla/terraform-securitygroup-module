211181a92420747f82a1652b03a06fa957b4af0a
maha <maridayya@gmail.com>
2026-01-02 00:58:15 +0530

    terraform security group module

--- main.tf
+++ main.tf
@@ -22,22 +22,3 @@ resource "aws_security_group" "allow_tls" {
 }
 
 
-#here we need to get sg name from user of this module. so declare it as a mandatory variable in variable.tf file
-
-# description can be optional so create a variable for description as optional
-
-#and VPC id we need to get it from vpc module 
-# which indicates that SG is dependent on VPC module. As a platform engineer when we develop a VPC module it's our responsibilty
-# to give that vpc id as output
-
-# so person who develops vpc will get vpc id as their result . we need to store this id somewhere so that whenver 
-# SG team trying to create SG they will take from that place.
-
-# AWS being platform between multiple resources it provides a feature called SSM parameter store
-
-#so whenver VPC developer creates VPC , he will store that id in parameter store and SG team will access id from parameter
-# strore whenver it is necessary
-
-# we need to access sg id from parameter store using data sources
-
-# tags as usual follow our regular process and give proper tags(merge commmon tags, name of SG and give an option to user if he wants to add any tags)
--- variables.tf
+++ variables.tf
@@ -1,11 +1,9 @@
-#when u just declare variable without any default then it will become mandatory value to be supplied by user. 
-#terraform will ask as prompt or else show error if user didn't provide this value.
+
 
 variable "sg_name" {
   type = string
 }
 
-#if we declare variable value with default then terraform will not give any error even if user dont provide a value.
 
 variable "sg_description" {
   type = string
